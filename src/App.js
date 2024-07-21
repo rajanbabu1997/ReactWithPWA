77a7252dbcfb147e7126b19cc3b7b14c2867bb96
Rajanbabu <rajanbabu4111997@gmail.com>
2024-07-21 14:07:09 +0530

    icon changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import GridView from './GridView';
 
 function App() {
   const [Productvisible, setProductvisible]= useState(false);  
+  const [Buttonvisible, setButtonvisible]= useState(false); 
   function onloadDate(){    
     setProductvisible(true);
+    setButtonvisible(true);
   }
   return (
-    <div>
-     <button onClick={onloadDate}> loadButton</button>
+    <div className='App'>
+      <div hidden={Buttonvisible}>Welcome to my page</div>
+     <button onClick={onloadDate} hidden={Buttonvisible}>Click here to load Data</button>
      { Productvisible ? <GridView/> : <div></div> }
     </div>
     
--- src/GridView.js
+++ src/GridView.js
@@ -2,18 +2,29 @@ import React, { useState, useEffect } from 'react';
 import ProductView from './ProductView';
 const GridView = () => {
     const [Products, setProducts] = useState([]);
+    const [viewProduct, setViewProduct] = useState(false);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((json) => {console.log(json); setProducts(json); console.log(Products)});
+      .then((json) => {console.log(json); setProducts(json); setViewProduct(true); console.log(Products)});
   }, []);
 
   return (
+    <div>
+    { viewProduct ?
        <div>
-      {Products.map((product) => (
-       <ProductView Product = {product}></ProductView>
-      ))}
+        {
+        Products.map((product) => (
+            <ProductView Product = {product}></ProductView>
+           ))
+        }
+    </div>:
+    <div>
+      Loading.........
+      <div class="loader"></div>
+    </div>
+}
     </div>
   )
 }
